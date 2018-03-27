c57342eb7a42d5587738b652d4c61b7fa125b5b1
mmcglone <mike@hbrisk.com>
2018-03-27 12:46:30 -0700

    fix-then-if: Fixes issue with thenIf

--- spec/thenIfSpec.js
+++ spec/thenIfSpec.js
@@ -1,16 +1,20 @@
 const thenIf = require('../thenIf');
 
 const c = a => a > 5;
-const f = a => a + 3;
+const f = p => p.then(value => value + 1);
 
 describe('thenIf', () => {
   describe('when passed a condition c, function f, and promise p that resolves to v', () => {
     describe('when c(v) is true', () => {
-      it('should return a promise that resolve to f(v)', () => {
+      it('should return a promise that resolves to f(p)', () => {
         const v = 8;
-        const returned = thenIf(c, f, Promise.resolve(v));
+        const p = Promise.resolve(v);
+        const returned = thenIf(c, f, p);
+        const expected = f(p);
         returned.then((value) => {
-          expect(value).toBe(f(v));
+          expected.then((value1) => {
+            expect(value).toBe(value1);
+          });
         });
       });
     });
@@ -24,7 +28,7 @@ describe('thenIf', () => {
       });
     });
     describe('when passed a condition c, function f, and promise p that rejects with r', () => {
-      it('should return a promise that rejects with', () => {
+      it('should return a promise that rejects with r', () => {
         const r = new Error('Reason for rejection');
         const returned = thenIf(c, f, Promise.reject(r));
         returned.catch((reason) => {
